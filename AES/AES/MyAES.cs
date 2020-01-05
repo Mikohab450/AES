f0d969c56037757512c72ad16e4bfb9061b11454
mikohab450 <yoloMixQ@gmail.com>
2020-01-05 14:14:34 +0100

    fix

--- AES/AES/MyAES.cs
+++ AES/AES/MyAES.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,99 @@ namespace AES
 {
     class MyAES
     {
+        private byte[] buffer;
+        private byte[] Key;
+        private byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+        private byte[] Encrypt(byte[] toEncrypt)
+        {
+            Aes myAes = Aes.Create();
+            myAes.GenerateKey();
+            //  myAes.GenerateIV();
+            Key = myAes.Key;
+            myAes.IV = IV;
+            Key = myAes.Key;
+            string sampleText = Encoding.UTF8.GetString(toEncrypt);
+            // Check arguments.
+            if (sampleText == null || sampleText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (myAes.Key == null || myAes.Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (myAes.IV == null || myAes.IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = myAes.Key;
+                aesAlg.IV = myAes.IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(sampleText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            buffer = encrypted;
+            return encrypted;
+        }
+        public byte[] Decrypt(byte[] toDecrypt) {
+            string sampleText = Encoding.UTF8.GetString(toDecrypt);
+
+            if (sampleText == null || sampleText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                //    var key = new Rfc2898DeriveBytes(, , 1000);
+                //  AES.Key = key.GetBytes(AES.KeySize / 8);
+                //AES.IV = key.GetBytes(AES.BlockSize / 8);
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                aesAlg.Padding = PaddingMode.Zeros;
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(buffer))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+
+                    }
+                }
+
+            }
+            return Encoding.UTF8.GetBytes(plaintext);
+        }
     }
 }
